59440ced80faea15a29d008d6c7ce459ddda7a30
Kgaugelo mahasha <kgaugelo.m@yahoo.com>
2023-03-21 14:15:21 +0200

    adding current weather card information

--- src/components/WeatherWrapper.js
+++ src/components/WeatherWrapper.js
@@ -1,12 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import LocationInput from "./location/LocationInput";
-import "./WeatherWarpper.css";
+import "./WeatherWrapper.css";
 import CurrentWeatherCard from "./weatherCard/currentWeatherCard/CurrentWeatherCard";
 import WeatherGraph from "./weatherGraph/WeatherGraph";
 import ForecastWeatherWrapper from "./weatherCard/forecastWeatherCard/ForecastWeatherWrapper";
 
 const WeatherWrapper = () => {
-  const [location, setLocation] = useState("Johannesburg")
+  const [location, setLocation] = useState("Johannesburg");
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&&appid=01b7b4f97ada9006a27f7aa192d2e0c8`;
+
+    fetch(apiUrl).then(response => response.json()).then(data => {
+      setWeather((prevState => {
+        prevState = {
+          condition: data.weather[0].description,
+          temperature: data.main.temp,
+          humidity: data.main.humidity,
+          pressure: data.main.pressure
+        };
+        return prevState
+      }));
+    }).catch(error => {
+      console.log(error);
+    });
+  }, [location]);
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -16,7 +35,7 @@ const WeatherWrapper = () => {
     if (event.key === 'Enter') {
       setLocation((prevState => {
         prevState = event.target.value;
-        return prevState
+        return prevState;
       }));
     }
   }
@@ -29,7 +48,7 @@ const WeatherWrapper = () => {
         </div>
         <div className={"contain-current-graph"}>
           <div className={"contain-current-graph_child"}>
-            <CurrentWeatherCard/>
+            <CurrentWeatherCard location={location} weather={weather}/>
           </div>
           <div className={"contain-current-graph_child"}>
             <WeatherGraph/>
@@ -40,7 +59,7 @@ const WeatherWrapper = () => {
         </div>
       </div>
     </form>
-  )
+  );
 }
 
 export default WeatherWrapper;
